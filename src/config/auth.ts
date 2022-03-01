c238cc3892d210e9018a7aa1685d5441d4338e5f
Evandro Monteiro <goodvandro@gmail.com>
2022-03-01 11:40:52 +0000

    implementing refresh_tooken in login

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: 'dd8b8adc3695c7f6d77bcbb2e9e0f098',
+  expires_in_token: '15m',
+  secret_refresh_token: '4a15739c26f90b80ff37b5ad06aea9ba',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
