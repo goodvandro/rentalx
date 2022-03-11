49a43b5e986b1575206561551fb579fb14c8bd09
Evandro Monteiro <goodvandro@gmail.com>
2022-03-11 14:24:11 +0000

    reset password

--- src/shared/infra/http/routes/passwords.routes.ts
+++ src/shared/infra/http/routes/passwords.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { SendForgotPasswordMailController } from '@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController';
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserControllerController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', sendForgotPasswordMailController.handle);
+passwordRoutes.post('/reset', resetPasswordUserControllerController.handle);
 
 export { passwordRoutes };
